cce91cd70869605c93bebb9c3f8df2997d899813
brillouinzone <david@stoker.org>
2024-06-19 01:06:47 -0700

    add setup file

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,12 @@
 import os
-from PyPHX.common import *
-from PyPHX.phx_os import *
+from PyPHX import *
 
 # Load the DLL
-directory="C:/Users/Public/Documents/Active Silicon/ActiveSDK v01.08.12/examples/build/windows/output/x64_Release"
-dll_path = os.path.abspath(os.path.join(directory,'phx_live_dll.dll'))
+
+
+localpath= os.path.dirname(os.path.realpath(__file__))
+# Load the DLL
+dll_path = os.path.abspath(os.path.join(localpath,'..','build','x64_Release', 'phx.dll'))
 phxdll = ctypes.CDLL(dll_path)
 
 
